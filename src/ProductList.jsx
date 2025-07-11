83214ee5e2bdf53ea617be1363848b68dce73a02
superUserT <rantshothabisomail@gmail.com>
2025-07-12 00:40:58 +0200

    updated reducers

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./ProductList.css";
 import CartItem from "./CartItem";
-import { addItem } from "./CartSlice";
+import { addItem, removeItem, updateQuantity } from "./CartSlice";
 
 const handleAddToCart = (product) => {
   dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
